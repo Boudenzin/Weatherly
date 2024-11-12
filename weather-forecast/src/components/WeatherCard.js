2ef78558177d0e852b81041af4f345408b28cbea
romildo <romildo-rrj@hotmail.com>
2024-11-12 16:26:55 -0300

    Flexibilização do WeatherCard para receber o getWeatherIcon

--- weather-forecast/src/components/WeatherCard.js
+++ weather-forecast/src/components/WeatherCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './WeatherCard.css';
 
+
 function WeatherCard({ cidade, temperatura, descricao, icon }) {
+
     return (
         <div class="weather-card">
             <h2>{cidade}</h2>
             <div className="weather-info">
-                <img src={icon} alt={descricao} className="weather-icon" />
+                <img src={icon} alt="Ícone do clima" className="weather-icon" />
                 <p>{temperatura}°C</p>
             </div>
             <p>{descricao}</p>
