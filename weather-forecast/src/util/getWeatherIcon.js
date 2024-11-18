315bcd15084a8fd4441f6d1b7bf7b92adf389103
romildo <romildo-rrj@hotmail.com>
2024-11-18 15:49:16 -0300

    Finalização do Filtro de ícone

--- weather-forecast/src/util/getWeatherIcon.js
+++ weather-forecast/src/util/getWeatherIcon.js
@@ -4,13 +4,18 @@ export function getWeatherIcon(description, isDaytime) {
   
   if (desc.includes("céu limpo")) {
     return isDaytime ? "/assets/3d-weather-icons/sun/26.png" : "/assets/3d-weather-icons/moon/10.png";
-  } else if (desc.includes("chuva")) {
+  } else if (desc.includes("chuva leve")) {
     return isDaytime ? "/assets/3d-weather-icons/sun/9.png" : "/assets/3d-weather-icons/moon/1.png";
-  } else if (desc.includes("nublado") || (desc.includes)("nuvens dispersas")) {
+  } else if (desc.includes("parcialmente nublado") || desc.includes("nuvens dispersas") || desc.includes("algumas nuvens")  || desc.includes("nublado")) {
     return isDaytime ? "/assets/3d-weather-icons/sun/27.png" : "/assets/3d-weather-icons/moon/15.png";
   } else if (desc.includes("tempestade")) {
     return isDaytime ? "/assets/3d-weather-icons/sun/28.png" : "/assets/3d-weather-icons/moon/20.png";
-  }
+
+  } else if (desc.includes("chuva forte")) {
+    return isDaytime ? "/assets/3d-weather-icons/sun/13.png" : "/assets/3d-weather-icons/moon/2-1.png";
+  } else if (desc.includes("céu coberto")) {
+    return "/assets/3d-weather-icons/cloud/35.png";
+  } 
   // Ícone padrão
   return "/assets/3d-weather-icons/snow/36.png";
 }
