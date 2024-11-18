48b2560513609aac389055ce961e57d74c799b3b
romildo <romildo-rrj@hotmail.com>
2024-11-18 16:31:24 -0300

    Adaptação do Weather Card

--- weather-forecast/src/components/WeatherCard.js
+++ weather-forecast/src/components/WeatherCard.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React from 'react'; 
 import './WeatherCard.css';
 
-
-function WeatherCard({ cidade, temperatura, descricao, icon }) {
-
-    return (
-        <div class="weather-card">
-            <h2>{cidade}</h2>
-            <div className="weather-info">
-                <img src={icon} alt="Ícone do clima" className="weather-icon" />
-                <p>{temperatura}°C</p>
-            </div>
-            <p>{descricao}</p>
-
+function WeatherCard({ cidade, temperatura, descricao, icon, vento, umidade }) {
+  return (
+    <div className="weather-card">
+      <h2>{cidade}</h2>
+      <div className="weather-info">
+        <img src={icon} alt="Ícone do clima" className="weather-icon" />
+        <p className="temperature">{temperatura}°C</p>
+        <div className="weather-extra">
+          <p>Vento: {vento}</p>
+          <p>Umidade: {umidade}</p>
         </div>
-    );
+      </div>
+      <p className="description">{descricao}</p>
+    </div>
+  );
 }
-
 
 export default WeatherCard;
